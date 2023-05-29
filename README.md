1849aa8b06c0cb6505c5f385f4c08b899a532a68
Avel Docquin <adocquin@outlook.com>
2023-05-29 10:17:30 +0200

    Updated test docstring. Added link to Medium article.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medium Unit Testing
+
+Link to Medium article: https://medium.com/@adocquin/mastering-unit-tests-in-python-with-pytest-a-comprehensive-guide-896c8c894304
--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -10,7 +10,8 @@ def test_divide_positive_numbers() -> None:
 
 def test_divide_negative_numbers() -> None:
     """
-    Test that divide returns the correct result when given a positive number and a negative number.
+    Test that divide returns the correct result when given a positive number and
+    a negative number.
     """
     assert divide(5, -2) == -2.5
     assert divide(-2, 5) == -0.4
